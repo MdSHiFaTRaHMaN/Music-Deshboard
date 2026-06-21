f4ee2289ba5f858df1b1b616ef48d4bac23d8cd8
shakilsarkar1412-code <shakil.sarkar1412@gmail.com>
2026-06-21 16:29:11 +0600

    implement authentication middleware to protect routes with JWT verification

--- src/middleware.js
+++ src/middleware.js
@@ -46,11 +46,12 @@ export const config = {
   matcher: [
     /*
      * Match all request paths except for the ones starting with:
+     * - api (API routes)
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      * - public files (images etc)
      */
-    "/((?!_next/static|_next/image|favicon.ico|images|images/).*)",
+    "/((?!_next/static|_next/image|favicon.ico|images).*)",
   ],
 };
